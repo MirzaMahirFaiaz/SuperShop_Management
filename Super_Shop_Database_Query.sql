8989d5bbe66402fca4b89997693ffcf8d8c1ffdd
RedowanRahman <redowanrahman1246@gmail.com>
2022-08-14 19:18:34 +0600

    Login Works
    jar file and query file added

---
+++ Super_Shop_Database_Query.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Super_Shop
+
+USE Super_Shop
+
+CREATE TABLE Users(
+	UserName varchar(50) primary key not null,
+	Pass_word varchar(50) not null,
+	UserType varchar(50) check (UserType='admin' or UserType='seller') not null 
+)
+
+
+INSERT INTO Users VALUES ('admin','admin','admin')
+
+
+select * from Users
